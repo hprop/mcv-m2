f859a1c16a5dbc9cf2808243082f3b72ca76c5bf
Hugo Prol <hprol@innovacs.es>
2017-10-24 00:08:36 +0200

    Add Gauss-Seidel method
    As an alternative to the analitycal solution for: Ax = b

---
+++ week2/sol_Poisson_Equation_GaussSeidel.m
@@ -0,0 +1,37 @@
+function [result] = sol_Poisson_Equation_GaussSeidel(f, dom, param)
+% dom -> logical matrix, if 1 then that pixel belongs to the domain
+% param.iterations -> number of iterations applied by the algorithm
+
+[ni, nj]=size(f);
+
+%We add the ghost boundaries (for the boundary conditions)
+f_ext = zeros(ni+2, nj+2);
+f_ext(2:end-1, 2:end-1) = f;
+dom_ext =zeros(ni+2, nj+2);
+dom_ext(2:end-1, 2:end-1) = dom;
+
+% Add padding to the driving parameter
+b = zeros(ni+2, nj+2);
+b(2:end-1, 2:end-1) = param.driving;
+
+if (isfield(param, 'iterations'))
+    iter = param.iterations;
+else
+    iter = 200;
+end
+
+for n=1:iter
+    for i=1:ni  % TODO: boundary in the image border??
+        for j=1:nj
+            if dom_ext(i, j) == 1
+                n = f_ext(i-1, j);
+                e = f_ext(i, j+1);
+                s = f_ext(i+1, j);
+                w = f_ext(i, j-1);
+                f_ext(i, j) = (-b(i, j) + n + e + s + w) / 4;
+            end
+        end
+    end
+end
+
+result = f_ext(2:end-1, 2:end-1);
