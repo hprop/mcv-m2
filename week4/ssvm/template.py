ebe0926a11daf301bb5bda45924c7f51102bff8f
Hugo Prol <hprol@innovacs.es>
2017-11-09 20:20:13 +0100

    Training/test for s-svm and svm models

--- week4/ssvm/template.py
+++ week4/ssvm/template.py
@@ -25,7 +25,7 @@ plot_labeling = False
 plot_coefficients = False
 
 
-""" 
+"""
 Load the segments and the groundtruth for all jackets
 """
 path_measures = 'man_jacket_hand_measures.xls'
@@ -52,11 +52,11 @@ n = 2
 plot_segments(segments[n],sheet.ide[n],labels_segments[n])
 
 
-""" 
-Make matrices X of shape (number of jackets, number of features) 
-and Y of shape (number of jackets, number of segments) where, 
+"""
+Make matrices X of shape (number of jackets, number of features)
+and Y of shape (number of jackets, number of segments) where,
 for all jackets,
-    X = select the features for each segment 
+    X = select the features for each segment
     Y = the grountruth label for each segment
 """
 Y = labels_segments
@@ -64,7 +64,7 @@ num_jackets = labels_segments.shape[0]
 num_labels = np.unique(np.ravel(labels_segments)).size
 
 """ CHANGE THIS IF YOU CHANGE NUMBER OF FEATURES """
-num_features = 7 
+num_features = 7
 X = np.zeros((num_jackets, num_segments_per_jacket, num_features))
 
 for jacket_segments, i in zip(segments, range(num_jackets)):
@@ -87,19 +87,20 @@ print('X, Y done')
 if add_gaussian_noise_to_features:
     print('Noise sigma {}'.format(sigma_noise))
     X = X + np.random.normal(0.0, sigma_noise, size=X.size).reshape(np.shape(X))
-      
+
 
 """
 DEFINE HERE YOUR GRAPHICAL MODEL AND CHOOSE ONE LEARNING METHOD
 (OneSlackSSVM, NSlackSSVM, FrankWolfeSSVM)
-"""      
-    
+"""
+model = ChainCRF()
+crf = FrankWolfeSSVM(model=model, C=.1)  # , max_iter=11)
 
-""" 
-Compare SVM with S-SVM doing k-fold cross validation, k=5, see scikit-learn.org 
+"""
+Compare SVM with S-SVM doing k-fold cross validation, k=5, see scikit-learn.org
 """
 n_folds = 5
-""" with 5 in each fold we have 4 jackets for testing, 19 for training, 
+""" with 5 in each fold we have 4 jackets for testing, 19 for training,
 with 23 we have leave one out : 22 for training, 1 for testing"""
 scores_crf = np.zeros(n_folds)
 scores_svm = np.zeros(n_folds)
@@ -108,7 +109,7 @@ wrong_segments_svm = []
 
 kf = KFold(num_jackets, n_folds=n_folds)
 fold = 0
-for train_index, test_index in kf: 
+for train_index, test_index in kf:
     print(' ')
     print('train index {}'.format(train_index))
     print('test index {}'.format(test_index))
@@ -118,14 +119,21 @@ for train_index, test_index in kf:
     Y_train = Y[train_index]
     X_test = X[test_index]
     Y_test = Y[test_index]
-                             
+
     """ YOUR S-SVM TRAINING CODE HERE """
+    crf.fit(X_train, Y_train)
 
-    
-    
     """ LABEL THE TESTING SET AND PRINT RESULTS """
+    Y_pred = crf.predict(X_test)
+    Y_pred = np.vstack(Y_pred)
 
-    
+    fails = np.sum(Y_pred != Y_test)
+    score = 1 - (float(fails) / (Y_test.shape[0] * Y_test.shape[1]))
+    wrong_segments_crf.append(fails)
+    scores_crf[fold] = score
+
+    print('CRF - total wrong segments: {}, score: {}'.format(fails, score))
+
     """ figure showing the result of classification of segments for
     each jacket in the testing part of present fold """
     if plot_labeling:
@@ -133,19 +141,35 @@ for train_index, test_index in kf:
             print(ti)
             print(pred)
             s = segments[ti]
-            plot_segments(s,caption='SSVM predictions for jacket '+str(ti+1),
+            plot_segments(s,
+                          caption='SSVM predictions for jacket ' + str(ti+1),
                           labels_segments=pred)
 
+    """ LINEAR SVM TRAINING AND TESTING """
 
-    """ YOUR LINEAR SVM TRAINING CODE HERE """
+    # Prepare data for svm (expected matrix with row-vector features)
+    X_train = np.vstack(X_train)
+    X_test = np.vstack(X_test)
+    Y_train = Y_train.flatten()
+    Y_test = Y_test.flatten()
 
+    # Create and train
+    svm = LinearSVC(dual=False, C=.1)
+    svm.fit(X_train, Y_train)
 
-    """ LABEL THE TESTING SET AND PRINT RESULTS """
+    # Use test dataset to meassure the svm's accuracy
+    Y_pred = svm.predict(X_test)
 
+    fails = np.sum(Y_pred != Y_test)
+    score = 1 - float(fails) / Y_test.size
+    wrong_segments_svm.append(fails)
+    scores_svm[fold] = score
+
+    print('SVM - total wrong segments: {}, score: {}'.format(fails, score))
 
     fold += 1
-    
-        
+
+
 """
 Global results
 """
@@ -170,7 +194,7 @@ print('Final score SVM: {}, {} wrong labels in total out of {}'.\
 
 if plot_coefficients:
     name_of_labels = [
-        'neck',		
+        'neck',
         'left shoulder',
         'outer left sleeve',
         'left wrist',
@@ -186,11 +210,7 @@ if plot_coefficients:
 
     """ SHOW IMAGE OF THE LEARNED UNARY COEFFICIENTS, size (num_labels, num_features)"""
     """ use matshow() and colorbar()"""
-    
-    
-    
+
+
+
     """ SHOW IMAGE OF PAIRWISE COEFFICIENTS size (num_labels, num_labels)"""
-
-
-           
-            
