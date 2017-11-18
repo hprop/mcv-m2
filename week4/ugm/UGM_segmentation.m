0fd76bf04ccc11c260bfda51ee1ce67fc4f09500
jordi-bird <jordi.puyoles.calafell@gmail.com>
2017-11-18 13:17:37 +0100

    Graph Cuts

--- week4/ugm/UGM_segmentation.m
+++ week4/ugm/UGM_segmentation.m
@@ -6,11 +6,11 @@ im_name='3_12_s.bmp';
 
 % TODO: Update library path
 % Add  library paths
+
 basedir='UGM';
+maxflow='maxflow';
 addpath(genpath(basedir));
-%addpath(basedir);
-
-
+addpath(genpath(maxflow));
 
 %Set model parameters
 %cluster color
@@ -72,6 +72,11 @@ if ~isempty(edgePot)
     im_bp= reshape(mu_color(decodeLBP,:),size(im));
     toc;
     
+    display('Graph Cuts'); tic;
+    decodeGC = UGM_Decode_GraphCut(nodePot,edgePot,edgeStruct);
+    im_gc= reshape(mu_color(decodeGC,:),size(im));
+    toc;
+    
     
     % TODO: apply other inference algorithms and compare their performance
     %
@@ -79,11 +84,11 @@ if ~isempty(edgePot)
     % - Linear Programing Relaxation
     
     figure
-    subplot(2,2,1),imshow(Lab2RGB(im));xlabel('Original');
-    subplot(2,2,2),imshow(Lab2RGB(im_c),[]);xlabel('Clustering without GM');
-    subplot(2,2,3),imshow(Lab2RGB(im_bp),[]);xlabel('Max-Sum');
-    subplot(2,2,4),imshow(Lab2RGB(im_lbp),[]);xlabel('Loopy Belief Propagation');
-    
+    subplot(2,3,1),imshow(Lab2RGB(im));xlabel('Original');
+    subplot(2,3,2),imshow(Lab2RGB(im_c),[]);xlabel('Clustering without GM');
+    subplot(2,3,3),imshow(Lab2RGB(im_bp),[]);xlabel('Max-Sum');
+    subplot(2,3,4),imshow(Lab2RGB(im_lbp),[]);xlabel('Loopy Belief Propagation');
+    subplot(2,3,5),imshow(Lab2RGB(im_gc),[]);xlabel('Graph Cuts');
 else
    
     error('You have to implement the CreateGridUGMModel.m function');
